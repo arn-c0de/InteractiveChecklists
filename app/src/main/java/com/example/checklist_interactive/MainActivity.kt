2e1649eef18c8c48a307eb8a30d60bc9f5d68a4c
arn-c0de <arn-c0de@protonmail.com>
2025-12-29 15:23:25 +0100

    init

--- app/src/main/java/com/example/checklist_interactive/MainActivity.kt
+++ app/src/main/java/com/example/checklist_interactive/MainActivity.kt
@@ -60,7 +60,7 @@ import java.util.Locale
 
 class MainActivity : ComponentActivity() {
         companion object {
-            const val SOFTWARE_VERSION = "1.0.18"
+            const val SOFTWARE_VERSION = "1.0.19"
         }
 
         val softwareVersion = SOFTWARE_VERSION
