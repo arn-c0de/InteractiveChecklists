4789894765e258c791ec0ef2f9e76fc22fb120b9
arn-c0de <arn-c0de@protonmail.com>
2026-01-15 13:44:21 +0100

    small fixes | changlog | 1.0.24

--- app/src/main/java/com/example/checklist_interactive/MainActivity.kt
+++ app/src/main/java/com/example/checklist_interactive/MainActivity.kt
@@ -61,7 +61,7 @@ import com.example.checklist_interactive.ui.setup.SetupWizardScreen
 
 class MainActivity : ComponentActivity() {
         companion object {
-            const val SOFTWARE_VERSION = "1.0.23"
+            const val SOFTWARE_VERSION = "1.0.24"
         }
 
         val softwareVersion = SOFTWARE_VERSION
